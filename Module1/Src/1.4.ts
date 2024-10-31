596c562dd4f62ca3738d6140c7ce3472cf0e3104
ESRAILHAQUE <esrailbblhs@gmail.com>
2024-10-31 12:05:07 +0600

    add

--- Module1/Src/1.4.ts
+++ Module1/Src/1.4.ts
@@ -1,4 +1,8 @@
 // Basic Data Types
 
 // String
-
+let FistName: string = "esrail";
+let age: number = 20;
+let x: undefined = undefined;
+let y: null = null;
+let isAdmin: boolean = true;
---
+++ Module1/dist/1.4.js
@@ -0,0 +1,8 @@
+"use strict";
+// Basic Data Types
+// String
+let FistName = "esrail";
+let age = 20;
+let x = undefined;
+let y = null;
+let isAdmin = true;
